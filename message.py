1ed910c5f8d208b2a0dbc4aac0c41243089bab84
shirleyng92 <kah.shirley@gmail.com>
2018-10-30 12:15:52 +0800

    paycheck

---
+++ message.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 30 12:04:20 2018
+
+@author: shirley.ng
+"""
+
+message = "Test Message"
+
+print(message.lower())
+print(message.upper())
+print(message.swapcase())
---
+++ paycheck.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 30 12:11:04 2018
+
+@author: shirley.ng
+"""
+
+salary = input("Please enter your salary")
+bonus = input("Please enter your bonus")
+#paycheck  =salary+bonus
+paycheck = int(salary)+int(bonus)
+print(paycheck)
