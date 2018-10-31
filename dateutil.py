26cae4a8e7ffc6e71ce697afa2c368c732811596
shirleyng92 <kah.shirley@gmail.com>
2018-10-31 10:25:24 +0800

    Date

---
+++ dateutil.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 31 10:10:56 2018
+
+@author: shirley.ng
+"""
+
+import datetime
+
+currentDate = datetime.date.today()
+print (currentDate)
+print (currentDate.year)
+print (currentDate.month)
+print (currentDate.day)
+
+print (currentDate.strftime('%d %b,%y'))
+print (currentDate.strftime('%d %b,%Y'))
