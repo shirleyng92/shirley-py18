2ea8f6e8d41554f8fcc5f0521f83708178454a09
shirleyng92 <kah.shirley@gmail.com>
2018-10-31 11:27:27 +0800

    conditional function

---
+++ conditionalfunction.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 31 11:18:23 2018
+
+@author: shirley.ng
+"""
+
+answer=input("Would you like express shipping? (yes/no) ")
+
+#indent does matter in python, it might caused error
+if answer == "yes" :
+    print("That will be an extra $10")
+    
+print("Thank you")
