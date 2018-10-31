32bc6db6231cf9c6ad45b5744ffb87df405a20f1
shirleyng92 <kah.shirley@gmail.com>
2018-10-31 11:39:37 +0800

    converting str to num

--- conditionalfunction.py
+++ conditionalfunction.py
@@ -5,10 +5,20 @@ Created on Wed Oct 31 11:18:23 2018
 @author: shirley.ng
 """
 
-answer=input("Would you like express shipping? (yes/no) ")
+#.lower change all input to lowercase
+answer=input("Would you like express shipping? (yes/no) ").lower()
 
 #indent does matter in python, it might caused error
 if answer == "yes" :
     print("That will be an extra $10")
     
 print("Thank you")
+
+#example 2
+deposit = input("How much would you like to deposit?")
+
+#use float to convert to integers & decimcals
+if float(deposit) > 100 :
+    print("You get a free toaster!")
+    
+print("Have a nice day")
