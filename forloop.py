0c480c1dd42204a01f6a998c02c2e820770890c3
shirleyng92 <kah.shirley@gmail.com>
2018-11-01 11:21:42 +0800

    For and while loop

---
+++ forloop.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  1 11:00:54 2018
+
+@author: shirley.ng
+"""
+
+#for steps in range(4):
+#for steps in range(1,10,2):
+#for steps in [1,2,3,4,5]:
+#for steps in ['red','green','blue','black']:
+for steps in ['red','green','blue','black',8]:
+    
+    print(steps)
---
+++ whileloop.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  1 11:13:13 2018
+
+@author: shirley.ng
+"""
+
+answer = 0
+
+while answer != 4:
+    answer=int(input("What is 2+2? "))
+    
+print("Yes! You are correct!")
