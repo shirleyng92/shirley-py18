c89105f01dda98c08809f4f102bb10a4fed6e97a
shirleyng92 <kah.shirley@gmail.com>
2018-11-21 11:46:36 +0800

    21st exercise

--- web/helloflask.py
+++ web/helloflask.py
@@ -5,12 +5,12 @@ Created on Wed Nov 21 10:19:24 2018
 @author: shirley.ng
 """
 
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route("/")
-def welcome():
-    return "Welcome Shirley! </p> <a href=http://127.0.0.1:5000/hello> Hello </a></p> <a href=http://127.0.0.1:5000/goodbye> Goodbye </a>"
+#@app.route("/")
+#def welcome():
+#    return "Welcome Shirley! </p> <a href=http://127.0.0.1:5000/hello> Hello </a></p> <a href=http://127.0.0.1:5000/goodbye> Goodbye </a>"
 
 @app.route("/hello")
 def hello():
@@ -19,6 +19,17 @@ def hello():
 @app.route("/goodbye")
 def goodbye():
     return "Goodbye Shirley!"
+
+@app.route("/")
+def formpage():
+    return render_template('formPage.html')
+
+@app.route("/submit",methods=['POST'])
+def submitpage():
+    txtValue = request.form['txtName']
+    print("received:" + txtValue);
+    ##return "<h1>Thank you for</h1>, :"
+    return render_template('submitPage.html',inputName=txtValue)
 
 if __name__=="__main__":
     app.run()
